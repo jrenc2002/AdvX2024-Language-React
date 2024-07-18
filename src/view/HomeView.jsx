4e8dd069b3fc74a67e7c3ecb8f2e0bf8cfcd2e52
OvOAuto <ovoauto.official@gmail.com>
2024-07-18 17:43:00 +0800

    send post test

--- src/view/HomeView.jsx
+++ src/view/HomeView.jsx
@@ -8,17 +8,10 @@ import { useNavigate } from 'react-router-dom';
 import { MessagePlugin } from 'tdesign-react';
 
 const HomeView = () => {
-<<<<<<< Updated upstream
-  const navigate = useNavigate()
-  const [_, setContentData] = useAtom(showContentAtom)
-  const [loaded, setLoaded] = useState(false)
-  const [recommend, setRecommend] = useState({ list: [] })
-=======
   const navigate = useNavigate();
   const [_, setContentData] = useAtom(showContentAtom);
   const [loaded,setLoaded]= useState(false);
   const [recommend,setRecommend] = useState({list: []});
->>>>>>> Stashed changes
 
   useEffect(() => {
     if (!loaded)
@@ -61,23 +54,12 @@ const HomeView = () => {
         </div> */}
         <div className="w-80 rounded-lg border border-gray-300 bg-white p-6 shadow-md dark:bg-gray-800">
           <h3 className="mb-2 pl-2 text-xl font-bold">帖子热榜</h3>
-<<<<<<< Updated upstream
-          {recommend.list.map((i) => (
-            <a href={'/post/' + i.id}>
-              标题：{i.title}
-              <em />
-              内容：{i.content}
-              <em />
-            </a>
-          ))}
-=======
           {recommend.list.map(i => <a href={'/post/' + i.id}>
             标题：{i.title}
             <br />
             内容：{i.content}
             <br />
           </a>)}
->>>>>>> Stashed changes
         </div>
       </div>
     </div>
